3474c86abbe016f422c401ab0b8e9199fa77ffa2
John Holt <jholtiv@gmail.com>
2025-11-29 16:48:48 +0700

    added cachyos kernel

--- files/scripts/cachy-kernel.sh
+++ files/scripts/cachy-kernel.sh
@@ -6,16 +6,12 @@ dnf -y remove \
     rm -r -f /usr/lib/modules/*
 
 # Enable repos
-dnf -y copr enable bieszczaders/kernel-cachyos-lto
-dnf -y copr enable bieszczaders/kernel-cachyos-addons
+dnf -y copr enable bieszczaders/kernel-cachyos
 
 dnf -y install --setopt=install_weak_deps=False \
-    kernel-cachyos-lto \
-    libcap-ng \
-    libcap-ng-devel \
-    procps-ng \
-    procps-ng-devel \
-    uksmd
+    kernel-cachyos
 
 # Clean up repos from earlier
 rm -f /etc/yum.repos.d/{*copr*}
+
+setsebool -P domain_kernel_load_modules on
